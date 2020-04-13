195137f66d6386ce813f4ba61d4ef126237412ac
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-04-13 17:14:23 -0600

    Gitignore because test.py is not useful for the code

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import geograpy4
 
-a = geograpy4.get_place_context("I really like Union Station in Toronto from Canada",addressOnly=True)
+a = geograpy4.get_place_context("The I like both the Union Station in New York and Washington in the United States",addressOnly=True)
 for result in a:
     print(result)
 
