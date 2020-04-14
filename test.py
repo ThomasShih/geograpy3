55faf00c71a4b39e26ef4cd3f4c37d166248dbcc
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-04-14 12:08:14 -0600

    Cleanup

--- test.py
+++
@@ -1,7 +0,0 @@
-import geograpy4
-
-a = geograpy4.get_place_context("St George's Square in London, United Kingdom is a masterpiece.",addressOnly=True,ignoreEstablishments=False)
-for result in a:
-    print(result)
-
-#geograpy4.get_place_context(text="Hi my name is Thomas and I am drinking at Bob's Tavern in Califorina")
