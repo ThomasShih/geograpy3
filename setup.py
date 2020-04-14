953038411387ef1a2907541906dc9abe884bd202
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-04-14 15:13:14 -0600

    Build

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ from setuptools import setup
 """Everything below is out of date, as of the fork by Thomas Shih"""
 try:
     import pypandoc
-    long_description = pypandoc.convert_file('README.md', 'rst')
-    print(long_description)
+    long_description = "Please check the README.md in https://github.com/ThomasShih/geograpy4."
 except Exception as e:
     print("Import Error: {}".format(e))
     long_description = open('README.md').read()
 
 setup(name='geograpy4',
-      version='1.0.1',
+      version='1.0.2',
       description='Extract locations from a URL or text and returns the geocoded data',
       long_description=long_description,
       url='https://github.com/ThomasShih/geograpy4',
